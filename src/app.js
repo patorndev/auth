7f2bb361f8a18c8688d0c165c23c5e13e9ca050f
patorndev <di_patorani@msn.com>
2017-06-27 20:03:41 +0700

    Lesson 71: Auth done, Add Spinner.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, Card, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+  state = { loggedIn: null};
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyA82OHLtBviJsrvC0tZFttslDKfI_npdxA',
@@ -15,12 +17,47 @@ class App extends Component {
       storageBucket: 'auth-967ee.appspot.com',
       messagingSenderId: '909940362483'
     });
+
+    firebase.auth().onAuthStateChanged((user) =>{
+      if (user) {
+        this.setState({ loggedIn: true });
+      }
+      else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Card>
+            <CardSection>
+              <Button onPress={() => firebase.auth().signOut()}>
+                Log Out
+              </Button>
+            </CardSection>
+          </Card>
+        );
+      case false:
+        return <LoginForm />
+      default:
+        return (
+          <Card>
+            <CardSection>
+              <Spinner size="large" />
+            </CardSection>
+          </Card>
+        );
+    };
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
